195d42faf03ce6bbb8083cea56a3ec7945e02b34
Stephan <sr27744@gmail.com>
2022-09-21 01:28:57 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const form = document.getElementById("contact-form")
+
+form.addEventListener("submit", function (e) {
+    const firstName = document.getElementById("firstName");
+    const lastName = document.getElementById("lastName");
+    const email = document.getElementById("email");
+    if(firstName.value == "" || lastName.value == "" || email.value == ""){
+        showValidationText();
+        alert("Please fill in all available fields");
+        e.preventDefault();
+        return false;
+    }
+    return true;
+})
+
+
+function showValidationText(){
+    const validationText1 = document.querySelector(".validation1");
+    const validationText2 = document.querySelector(".validation2");
+    const validationText3 = document.querySelector(".validation3");
+    validationText1.textContent = "Required field";
+    validationText2.textContent = "Required field";
+    validationText3.textContent = "Required field";
+}
+
+
+
+
+
